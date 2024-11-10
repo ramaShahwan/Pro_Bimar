909780dbf3d004b3c624a3fff79c80040d5d7bd1
fatimaalsalam <128462430+fatimaalsalam@users.noreply.github.com>
2024-11-10 14:16:35 +0200

    tyt

--- resources/views/user/bill_courses.blade.php
+++ resources/views/user/bill_courses.blade.php
@@ -1,20 +1,22 @@
 @extends('layout_user.mester')
 @section('content')
 <style>
-   
+
     .invoice-header {
         display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding-bottom: 20px;
-        border-bottom: 2px solid #f0f0f0;
+    justify-content: space-around;
+    /* margin-top: 20px; */
+    direction: rtl;
+    align-items: center;
+    /* padding-bottom: 20px; */
+    border-bottom: 2px solid #f0f0f0;
     }
     .invoice-header h1 {
         font-size: 24px;
         color: #0a3b92;
     }
     .company-logo {
-        width: 60px;
+        width: 100px;
     }
     .company-details {
         text-align: center;
@@ -31,6 +33,7 @@
         border-collapse: collapse;
         font-size: 14px;
         text-align: center;
+        direction: rtl;
     }
     thead th {
         background-color: #f0f0f0;
@@ -56,7 +59,7 @@
         color: #333;
     }
     .totals p {
-        margin: 5px 0;
+        margin: 20px 0;
     }
     .grand-total {
         color: #0a3b92;
@@ -70,69 +73,70 @@
         border-top: 2px solid #f0f0f0;
         padding-top: 10px;
     }
+    .gd{
+        background: #1c998b;
+    padding: 5px;
+    display: block;
+    width: 100px;
+    margin-bottom: 20px;
+    /* float: right; */
+    margin-left: 200px;
+    text-align: center;
+    color: white;
+    border-radius: 25px;
+    }
 </style>
 <div class="invoice-header">
     <h1>فاتورة</h1>
-    <img src="letter.png" alt="شعار الشركة" class="company-logo">
+    <img src="{{asset('assetss/re.png')}}" alt="شعار الشركة" class="company-logo">
 </div>
 
 <div class="company-details">
-    <p>شركة النسور</p>
-    <p>قاعة الملكة للمناسبات</p>
-    <p>رقم هاتف العميل: 012-1234-567</p>
-    <p>عنوان العميل: 75 شارع النصر، القاهرة</p>
+    <p>شركة بيمار</p>
+    <!-- <p>قاعة الملكة للمناسبات</p> -->
+    <p style="text-align: end;">  الاسم الكامل: احمد الاحمد </p>
+    <p style="text-align: end;"> رقم الايصال:7777   </p>
 </div>
 
 <div class="venue-details">
-    <p>قاعة الملكة للمناسبات</p>
+    <p>تفاصيل الفاتورة  </p>
 </div>
 
 <table>
     <thead>
         <tr>
-            <th>Photo</th>
-            <th>Item</th>
-            <th>Quantity/Hours</th>
-            <th>Unit Price</th>
-            <th>Total</th>
+            <th>اسم البرنامج</th>
+            <th>اسم الدورة التدريبية</th>
+            <th>السنة التدريبية</th>
+            <th>تاريخ التسجيل على الدورة </th>
+            <!-- <th>Total</th> -->
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td><img src="letter.png" alt="Confetti"></td>
-            <td>Confetti cannon</td>
-            <td>1</td>
-            <td>$123</td>
-            <td>$123</td>
+            <td>هندسة معلوماتية</td>
+            <td>اخصائي فني معلوماتية </td>
+            <td>2020</td>
+            <td>2024/2/12</td>
+            <!-- <td>$123</td> -->
         </tr>
-        <tr>
-            <td><img src="letter.png" alt="Banner"></td>
-            <td>Rainbow Banner</td>
-            <td>1</td>
-            <td>$123</td>
-            <td>$123</td>
-        </tr>
-        <tr>
-            <td><img src="letter.png" alt="Disco Ball"></td>
-            <td>Disco ball and lights</td>
-            <td>1</td>
-            <td>$123</td>
-            <td>$123</td>
-        </tr>
+
+
     </tbody>
 </table>
 
 <div class="totals">
-    <p>المجموع الفرعي: $492</p>
-    <p>الضريبة (0%): $0</p>
-    <p class="grand-total">إجمالي السعر: 492$</p>
+    <p> المبلغ المستحق فبل تطبيق الحسم:502205</p>
+    <p>قيمة الحسم (0%): 0</p>
+    <p class="grand-total"> المبلغ المستحق بعد تطبيق الحسم:505052</p>
+    <a href="" class="gd">خيار الطباعة</a>
 </div>
 
-<div class="bank-details">
+<!-- <div class="bank-details">
     <p>تفاصيل البنك: بنك القاهرة المركزي</p>
     <p>اسم العميل على الحوالة: شركة النسور</p>
     <p>رقم الحساب: 8565235</p>
-</div>
+</div> -->
 
 
 @endsection
