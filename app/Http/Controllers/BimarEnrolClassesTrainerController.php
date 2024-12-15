ba5fb92210840588f6163f26fb4cec994ed5f36b
ramaShahwan <rama692000@gmail.com>
2024-12-15 14:43:19 +0300

    edit

--- app/Http/Controllers/BimarEnrolClassesTrainerController.php
+++ app/Http/Controllers/BimarEnrolClassesTrainerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bimar_Enrol_Classes_Trainer;
-use App\Models\Bimar_Course_Enrol_Trainer;
+use App\Models\Bimar_User;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -30,6 +30,7 @@ class BimarEnrolClassesTrainerController extends Controller
     {
         if (Auth::guard('administrator')->check() || Auth::guard('operation_user')->check()) {
             $data = Bimar_Enrol_Classes_Trainer::where('bimar_enrol_class_id',$class_id)->get();
+            $users = Bimar_User::where('bimar_role_id',3)->where('bimar_users_status_id',1)->get();
             return view('admin.addtimecourse',compact('data'));
         }else{
             return redirect()->route('home');
@@ -58,7 +59,6 @@ class BimarEnrolClassesTrainerController extends Controller
                     return redirect()->back()->with('message',' لا يمكن اضافة نفس المعلومات المضافة مسبقاً');
                    }
             }
-
             $data = new Bimar_Enrol_Classes_Trainer;
             $data->tr_enrol_classes_trainer_desc = $request->tr_enrol_classes_trainer_desc;
             $data->bimar_course_enrollment_id = $request->bimar_course_enrollment_id;
