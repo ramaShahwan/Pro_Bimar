617d125a7277bddf3a42f29f6ce49b622b243f0c
fatimaalsalam <128462430+fatimaalsalam@users.noreply.github.com>
2024-12-22 13:31:10 +0200

    file

--- config/auth.php
+++ config/auth.php
@@ -55,7 +55,7 @@ return [
         'driver' => 'session',
         'provider' => 'bimar_users',
     ],
-
+   
     'trainee' => [
         'driver' => 'session',
         'provider' => 'bimar_trainees',
@@ -84,7 +84,7 @@ return [
     |
     */
 
-    
+
 'providers' => [
     'bimar_users' => [
         'driver' => 'eloquent',
@@ -98,7 +98,7 @@ return [
 ],
 
 
-        
+
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
---
+++ resources/views/trainer/addcourse_sessions.blade.php
@@ -0,0 +1,448 @@
+@extends('layout_admin.master')
+@section('content')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+<style>
+   body{
+    color: #403e3e;
+}
+.input-groupp-icon input {
+    text-align: end;
+    padding-right: 4.4em;
+}
+
+    .bbtn{
+        border: none;
+    padding: 10px;
+    background-color: #61baaf;
+    color: white;
+    border-radius: 20px;
+    }
+    .bttn:hover{
+        background-color: #61baaf;
+        color: white;
+        font-size: 17px;
+        font-weight: 600;
+    }
+    .popup .overlay{
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            width: 100vw;
+            height: 100vw;
+            background: rgba(0, 0, 0, 0.7);
+            z-index: 1;
+            display: none;
+        }
+        .popup .content{
+            /* position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%) scale(0);
+
+            width: 450px;
+            height: 220px;
+            z-index: 2;
+            text-align: center;
+            padding: 20px;
+            box-sizing: border-box; */
+            max-width: 38em;
+    padding: 1em 3em 2em 3em;
+    /* margin: 0em auto; */
+    background-color: #fff;
+    /* border-radius: 4.2px; */
+    /* box-shadow: 0px 3px 10px -2px rgba(0, 0, 0, 0.2); */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    background: #fff;
+    width: 450px;
+    /* height: 220px; */
+    z-index: 2;
+    text-align: center;
+    padding: 20px;
+    box-sizing: border-box;
+
+        }
+        .popup .close-btn{
+            cursor: pointer;
+            position: absolute;
+            right: 20px;
+            top: 20px;
+            width: 30px;
+            height: 30px;
+            background: #222;
+            color: #fff;
+            font-size: 25px;
+            font-weight: 600;
+            line-height: 30px;
+            text-align: center;
+            border-radius: 50%;
+        }
+        .popup.active .overlay{
+            display: block;
+        }
+        .popup.active .content{
+            transition: all 300ms ease-in-out;
+            transform: translate(-50%,-50%) scale(1);
+
+        }
+    .card {
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 1px solid rgba(0, 0, 0, .125);
+    border-radius: .25rem;
+}
+.row {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+}
+.card-header {
+    padding: .75rem 1.25rem;
+    margin-bottom: 0;
+    background-color: rgba(0, 0, 0, .03);
+    border-bottom: 1px solid rgba(0, 0, 0, .125);
+}
+.table td, .table th {
+    text-align: center;
+    font-size: 17px;
+    padding: .75rem !important;
+    vertical-align: top;
+    border-top: 1px solid #dee2e6;
+}
+.popup .overlay{
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            width: 100vw;
+            height: 100vw;
+            background: rgba(0, 0, 0, 0.7);
+            z-index: 1;
+            display: none;
+        }
+        .popup .content{
+            /* position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%) scale(0);
+
+            width: 450px;
+            height: 220px;
+            z-index: 2;
+            text-align: center;
+            padding: 20px;
+            box-sizing: border-box; */
+            max-width: 38em;
+    padding: 1em 3em 2em 3em;
+    /* margin: 0em auto; */
+    background-color: #fff;
+    /* border-radius: 4.2px; */
+    /* box-shadow: 0px 3px 10px -2px rgba(0, 0, 0, 0.2); */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    background: #fff;
+    width: 450px;
+    /* height: 220px; */
+    z-index: 2;
+    text-align: center;
+    padding: 20px;
+    box-sizing: border-box;
+
+        }
+        .popup .close-btn{
+            cursor: pointer;
+            position: absolute;
+            right: 20px;
+            top: 20px;
+            width: 30px;
+            height: 30px;
+            background: #222;
+            color: #fff;
+            font-size: 25px;
+            font-weight: 600;
+            line-height: 30px;
+            text-align: center;
+            border-radius: 50%;
+        }
+        .popup.active .overlay{
+            display: block;
+        }
+        .popup.active .content{
+            transition: all 300ms ease-in-out;
+            transform: translate(-50%,-50%) scale(1);
+
+        }
+          /* شكل المفتاح */
+          .switch {
+            position: relative;
+            display: inline-block;
+            width: 60px;
+            height: 34px;
+        }
+
+        /* شريط الخلفية */
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            transition: .4s;
+            border-radius: 34px;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            transition: .4s;
+            border-radius: 50%;
+        }
+
+        /* اللون الأخضر عند التفعيل */
+        input:checked + .slider {
+            background-color: green;
+        }
+
+        /* تحريك الزر عند التفعيل */
+        input:checked + .slider:before {
+            transform: translateX(26px);
+        }
+
+input[type="radio"]:checked + label,
+input:checked + label:active {
+  /* background-color: #f0a500; */
+  background-color: #61baaf;
+  color: #fff;
+  /* border-color: #bd8200; */
+  border-color: #61baaf;
+}
+.gg{
+    font-size: 20px;
+    border: none;
+    background: none;
+    border-radius: none;
+    color: #ff0404;
+    padding: 0;
+}
+.gg:hover{
+    font-size: 20px;
+    border: none;
+    background: none;
+    border-radius: none;
+    color: #ff0404;
+}
+</style>
+
+<div id="page-wrapper">
+@if(session()->has('message'))
+        <div class="alert alert-info" role="alert" style="text-align:end;font-size: 20px; ">
+          {{session()->get('message')}}
+        </div>
+@endif
+<div class="row" style="    margin: 80px 30px; direction: rtl;">
+            <div class="col-lg-12">
+                <div class="card">
+                        <div class="card-header" style="text-align: start;font-size: 20px;display: flex;justify-content: space-between;align-items: center;">
+                            <h3><i class="fa-sharp fa-solid fa-calendar-week"></i> الجلسات </h3>
+                            <!-- <a href="add.html" style="background: #007bff;padding: 6px;color: white;"><i class="las la-user-plus"></i> مدرب جديد</a> -->
+                            <button onclick="togglePopuo()" class="bbtn">اضافة جلسة</button>
+                        </div>
+                    <div class="card-block">
+                        <table class="table table-bordered table-striped table-condensed">
+                            <thead style="text-align: center;">
+                                <tr>
+                                    <th>الدورة التدريبية  </th>
+                                    <th>رقم الجلسة</th>
+
+                                    <th>عنوان الجلسة</th>
+                                    <th>تاريخ الجلسة</th>
+
+                                    <th>الأحداث</th>
+                                </tr>
+                            </thead>
+                            <tbody style="text-align: center;">
+
+                                <tr>
+                                <td>الايبتششت  </td>
+                                <td>الايبتششت  </td>
+
+                                    <td>ىتنسين </td>
+                                    <!-- <td><label class="switch">
+
+    <input type="checkbox" class="switch-button" data-id="{{ $call->tr_bank_status }}" {{ $call->tr_bank_status == 1 ? 'checked' : '' }}>
+    <span class="slider"></span>
+</label></td> -->
+<td>
+تابصتات نبنس
+</td>
+
+                                    <td>
+                                        <!-- <a href=""><span class="las la-trash-alt" style="font-size: 30px; color: #f00707;"></span></a> -->
+                                        <!-- <a href="{{url('type/edit',$call->tr_type_id)}}"><span class="las la-edit" style="font-size: 30px; color: #3f4046;"></span></a> -->
+                                        <!-- <button onclick="togglePopuoo()" style="border: none;background: none;"><span class="las la-edit" style="font-size: 30px; color: #3f4046;"></span> </button> -->
+                                        <!-- <a href="show.html"><span class="las la-eye" style="font-size: 30px; color: #1cda55;"></span></a> -->
+                                        <form action="{{url('general_content/destroy',$call->id)}}" method="post">
+                                        @csrf
+                                                <!-- <p class="fables-product-info my-2"><a  >
+
+                                                <span class="fables-btn-value">التسجيل على الكورس</span></a></p> -->
+                                                <input type="submit"  class="gg" style=" " value="X" onclick="return confirm('هل تريد الحذف')">
+                                                </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                        <!-- <nav>
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Prev</a>
+                                </li>
+                                <li class="page-item active">
+                                    <a class="page-link" href="#">1</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">2</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">3</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">4</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav> -->
+                    </div>
+                </div>
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <div class="popup" id="popup-1">
+            <div class="overlay"></div>
+            <div class="content">
+                <div class="close-btn" onclick="togglePopuo()">&times;</div>
+                <!-- <div class="containerr"> -->
+                <form action="{{url('general_content/store')}}" method="post" enctype="multipart/form-data">
+                @csrf
+                      <div class="roww">
+                        <h4>جلسة جديد </h4>
+
+
+                        <div class="input-groupp input-groupp-icon">
+                          <input type="text" placeholder="رقم الجلسة  " name="tr_course_session_arrangement" class="@error('tr_course_session_arrangement') is-invalid @enderror"/>
+                          <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                          @error('tr_course_session_arrangement')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                        </div>
+                        <div class="input-groupp input-groupp-icon">
+                          <input type="text" placeholder="عنوان الجلسة  " name="tr_course_session_desc" class="@error('tr_course_session_desc') is-invalid @enderror"/>
+                          <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                          @error('tr_course_session_desc')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                        </div>
+                        <div class="input-groupp input-groupp-icon">
+                          <input type="text" placeholder="تاريخ الجلسة  " name="tr_course_session_date" class="@error('tr_course_session_date') is-invalid @enderror"/>
+                          <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                          @error('tr_course_session_date')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                        </div>
+
+
+                      </div>
+
+                      <div class="roww">
+
+
+                        <input type="hidden" name="bimar_training_course_id" value="{{ $id }}">
+
+
+                      </div>
+                      <div class="roww">
+                       <input type="submit" value="حفظ" class="bttn">
+                      </div>
+                    </form>
+                  <!-- </div> -->
+
+            </div>
+        </div>
+
+
+</div>
+
+        <!-- /. PAGE WRAPPER  -->
+
+    <!-- /. WRAPPER  -->
+
+    <!-- /. FOOTER  -->
+    <script>
+        function togglePopuo(){
+            document.getElementById("popup-1").classList.toggle("active");
+        }
+        function togglePopuoo(){
+            document.getElementById("popuppo-1").classList.toggle("active");
+        }
+    </script>
+    <script>
+        // JavaScript to handle toggle switch behavior
+    document.querySelectorAll('.toggle-switch input').forEach((toggle) => {
+        toggle.addEventListener('change', function() {
+            if (this.checked) {
+                this.parentElement.parentElement.classList.add('highlight');
+            } else {
+                this.parentElement.parentElement.classList.remove('highlight');
+            }
+        });
+    });
+    //switch
+    document.querySelector('input[type="checkbox"]').addEventListener('change', function() {
+      if(this.checked) {
+        console.log("مفعل");
+      } else {
+        console.log("غير مفعل");
+      }
+    });
+
+    </script>
+
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    @endsection
