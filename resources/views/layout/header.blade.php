282ce8b7745ab3435bbed8a5ec02894334edb389
ramaShahwan <rama692000@gmail.com>
2025-01-07 13:56:10 +0300

    edit contact information

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -47,10 +47,10 @@
 
             </div>
             <div class="col-12 col-sm-5 col-lg-4 text-right">
-                <p class="fables-third-text-color font-13"><span class="fables-iconphone"></span> Phone : (888) 6000 6000</p>
+                <p class="fables-third-text-color font-13"><span class="fables-iconphone"></span> Phone : (011) 611 2040</p>
             </div>
             <div class="col-12 col-sm-5 col-lg-3 text-right">
-                <p class="fables-third-text-color font-13"><span class="fables-iconemail"></span> Email: Design@domain.com</p>
+                <p class="fables-third-text-color font-13"><span class="fables-iconemail"></span> Email: bimar@proengtrain.com</p>
             </div>
 
         </div>
