040a1bdba34fffef6f02bdb4a9070679d55b0d17
ramaShahwan <rama692000@gmail.com>
2025-01-15 11:51:50 +0300

    up

--- app/Http/Controllers/BimarQuestionsBankUserController.php
+++ app/Http/Controllers/BimarQuestionsBankUserController.php
@@ -89,14 +89,8 @@ class BimarQuestionsBankUserController extends Controller
     public function update(Request $request, $id)
     {
             if (Auth::guard('administrator')->check() || Auth::guard('operation_user')->check() ) {
-                $validated = $request->validate([
-                    'bimar_questions_bank_id' => 'required',
-                    'bimar_user_id' => 'required',
-                  ]);
-
+                
             $data = Bimar_Questions_Bank_User::findOrFail($id);
-            $data->bimar_questions_bank_id = $request->bimar_questions_bank_id;
-            $data->bimar_user_id = $request->bimar_user_id;
             $data->tr_questions_user_read = $request->tr_questions_user_read;
             $data->tr_questions_user_update = $request->tr_questions_user_update;
             $data->tr_questions_user_add = $request->tr_questions_user_add;
