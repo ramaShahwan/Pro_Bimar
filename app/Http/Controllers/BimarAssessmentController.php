5227af784e0e4d21c12fb73e35c702a41a1de750
ramaShahwan <rama692000@gmail.com>
2025-02-06 12:27:25 +0300

    edit

--- app/Http/Controllers/BimarAssessmentController.php
+++ app/Http/Controllers/BimarAssessmentController.php
@@ -46,7 +46,7 @@ class BimarAssessmentController extends Controller
     {
         if (Auth::guard('administrator')->check() || Auth::guard('operation_user')->check()) {
             $data::Bimar_User::where('bimar_users_status_id',1)->get();
-            return view('bank.addassessment',compact('data'));
+            return view('bank.addtrainerlink',compact('data'));
         }else{
             return redirect()->route('home');
         }
