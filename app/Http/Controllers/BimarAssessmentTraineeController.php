bc4d39cff9944ced65d6f6b6db6f87af2847a717
ramaShahwan <rama692000@gmail.com>
2025-02-26 12:54:24 +0300

    trainee

--- app/Http/Controllers/BimarAssessmentTraineeController.php
+++ app/Http/Controllers/BimarAssessmentTraineeController.php
@@ -280,7 +280,7 @@ dd($correctAnswers);
         ?? Auth::guard('trainer')->user()
         ?? Auth::guard('trainee')->user();
 
-        if (Auth::guard('trainer')->check()) {
+        if (Auth::guard('trainee')->check()) {
             $validated = $request->validate([
                 'bimar_assessment_id' => 'required',
             ]);
