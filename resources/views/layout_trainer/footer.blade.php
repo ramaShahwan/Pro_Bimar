74e196990ed7fa72583d72c012bb53484cb1220f
fatimaalsalam <128462430+fatimaalsalam@users.noreply.github.com>
2025-04-13 14:34:54 +0300

    update

--- resources/views/layout_trainer/footer.blade.php
+++ resources/views/layout_trainer/footer.blade.php
@@ -1,3 +1,3 @@
 <div id="footer-sec">
-        &copy; 2014 YourCompany | Design By : <a href="http://www.binarytheme.com/" target="_blank">BinaryTheme.com</a>
+        &copy; 2025, Design By : <a href="" target="_blank">BIMAR Company</a>
     </div>
